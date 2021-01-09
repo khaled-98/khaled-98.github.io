fc8dfd2e43a71c204adfc8823a54a809b12b6833
Khaled <khaledsoultan63@gmail.com>
2021-01-09 17:53:04 +0000

    animation continuation

--- assets/js/bayes_localisaton.js
+++ assets/js/bayes_localisaton.js
@@ -1,5 +1,18 @@
-function moveRight() {
-    window.alert(document.getElementById("robot").style)
-    // document.getElementById("robot").style.left = '50%'
-        // document.getElementById("robot").style.left + 10
+function moveRight(dir) {
+    const line = document.getElementById('line');
+    const line_width = parseFloat(getComputedStyle(line).width);
+
+    const robot = document.getElementById("robot");
+    let robot_pos = parseFloat(getComputedStyle(robot).left);
+    
+    const dist_to_move = line_width/10.5;
+    if(dir == 0)
+        robot_pos += dist_to_move;
+    else if(dir == 1)
+        robot_pos -= dist_to_move;
+
+    const ratio = robot_pos/line_width;
+    if((ratio > 0.95) || (ratio < 0.05))
+        return;
+    robot.style.left = robot_pos;
 }
