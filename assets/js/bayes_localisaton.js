ced11aa32941f6ee9fb1f6c6c584ab76440ee080
Khaled Saad <khaledsoultan63@gmail.com>
2021-01-09 17:12:50 +0000

    Bayesian animation

---
+++ assets/js/bayes_localisaton.js
@@ -0,0 +1,5 @@
+function moveRight() {
+    window.alert(document.getElementById("robot").style)
+    // document.getElementById("robot").style.left = '50%'
+        // document.getElementById("robot").style.left + 10
+}
