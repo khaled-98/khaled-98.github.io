3b034bd68114d50c76fee93cefb8525d0b657acc
Khaled <khaledsoultan63@gmail.com>
2021-01-17 01:12:50 +0000

    Refactored motion model js

--- assets/js/motion_model.js
+++ assets/js/motion_model.js
@@ -1,68 +1,82 @@
-var mousePosition;
 var offset = [0,0];
+class Robot
+{
+    constructor(robot_id, robot_center_id, robot_edge_id)
+    {
+        this.robot = document.getElementById(robot_id);
+        this.robot_center = document.getElementById(robot_center_id);
+        this.robot_edge = document.getElementById(robot_edge_id);
+        this.is_robot_center_down = false;
+        this.is_robot_edge_down = false;
 
-var isBeforeDown = false;
-var before_circle = document.getElementById('before');
-before_circle.style.left = '0px'
+        // Event listeners
+        var self = this;
+        this.robot_center.addEventListener('mousedown', function(e) {
+            self.is_robot_center_down = true;
+            offset = [
+                self.robot.offsetLeft - e.clientX,
+                self.robot.offsetTop - e.clientY
+            ];
+        }, true);
 
-var isAfterDown = false;
-var after_circle = document.getElementById('after');
-var after_circle_center = document.getElementById('after_center');
-var after_circle_edge = document.getElementById('after_edge');
-after_circle.style.left = '150px'
+        this.robot_edge.addEventListener('mousedown', function(e) {
+            self.is_robot_edge_down = true;
+        }, true);
+    }
+}
 
-// var draw = SVG().addTo('body')
-
-
-// draw pink square
-// var line = draw.line(0, 0, 100, 150).stroke({ width: 1 })
-// line.stroke({ color: 'black', width: 5, linecap: 'round'})
-// line.plot(50, 50, 100, 100)
-
-// var x1 = before_circle.style.left + (before_circle.style.width/2);
-// var y1 = before_circle.style.offset.top + (before_circle.style.height/2);
-// var x2 = after_circle.style.offset.left + (after_circle.style.width/2);
-// var y2 = after_circle.style.offset.top + (after_circle.style.height/2);
-// console.log(before_circle.contentDocument)
-
-// before_circle.addEventListener('mousedown', function(e) {
-//     isBeforeDown = true;
-//     offset = [
-//         before_circle.offsetLeft - e.clientX,
-//         before_circle.offsetTop - e.clientY
-//     ];
-// }, true);
-
-after_circle_center.addEventListener('mousedown', function(e) {
-    isAfterDown = true;
-    offset = [
-        after_circle.offsetLeft - e.clientX,
-        after_circle.offsetTop - e.clientY
-    ];
-}, true);
+let robot_before = new Robot("before", "before_center", "before_edge")
+let robot_after = new Robot("after", "after_center", "after_edge");
 
 document.addEventListener('mouseup', function() {
-    isBeforeDown = false;
-    isAfterDown = false;
+    robot_before.is_robot_center_down = false;
+    robot_before.is_robot_edge_down = false;
+    robot_after.is_robot_center_down = false;
+    robot_after.is_robot_edge_down = false;
 }, true);
 
 document.addEventListener('mousemove', function(event) {
     event.preventDefault();
-    if (isBeforeDown) {
-        mousePosition = {
-            x : event.clientX,
-            y : event.clientY
-        };
-        before_circle.style.left = (mousePosition.x + offset[0]) + 'px';
-        before_circle.style.top  = (mousePosition.y + offset[1]) + 'px';
+    const mousePosition = {
+        x : event.clientX,
+        y : event.clientY
+    };
+    if (robot_before.is_robot_center_down) 
+    {  
+        robot_before.robot.style.left = (mousePosition.x + offset[0]) + 'px';
+        robot_before.robot.style.top  = (mousePosition.y + offset[1]) + 'px';
+    }
+    else if (robot_before.is_robot_edge_down)
+    {
+        const box = robot_before.robot.getBoundingClientRect();
+        const centerX = box.left + box.width/2 - window.pageXOffset;
+        const centerY = box.top + box.height/2 - window.pageYOffset;
+        const rad = Math.atan2(event.clientX-centerX, event.clientY-centerY);
+        const deg = rad*(180/Math.PI)*-1 + 180;
+        rotateElement(robot_before.robot, deg)
     }
 
-    else if (isAfterDown) {
-        mousePosition = {
-            x : event.clientX,
-            y : event.clientY
-        };
-        after_circle.style.left = (mousePosition.x + offset[0]) + 'px';
-        after_circle.style.top  = (mousePosition.y + offset[1]) + 'px';
+    else if (robot_after.is_robot_center_down) 
+    {
+        robot_after.robot.style.left = (mousePosition.x + offset[0]) + 'px';
+        robot_after.robot.style.top  = (mousePosition.y + offset[1]) + 'px';
+    }
+    else if (robot_after.is_robot_edge_down)
+    {
+        const box = robot_after.robot.getBoundingClientRect();
+        const centerX = box.left + box.width/2 - window.pageXOffset;
+        const centerY = box.top + box.height/2 - window.pageYOffset;
+        const rad = Math.atan2(event.clientX-centerX, event.clientY-centerY);
+        const deg = rad*(180/Math.PI)*-1 + 180;
+        rotateElement(robot_after.robot, deg)
     }
 }, true);
+
+function rotateElement(elem, deg)
+{
+    elem.style.webkitTransform = 'rotate('+deg+'deg)'; 
+    elem.style.mozTransform    = 'rotate('+deg+'deg)';
+    elem.style.msTransform     = 'rotate('+deg+'deg)';
+    elem.style.oTransform      = 'rotate('+deg+'deg)';
+    elem.style.transform       = 'rotate('+deg+'deg)';
+}
