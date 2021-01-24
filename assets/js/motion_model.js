bd2a56c800b5f840aa13e14fbf263c7a8411df19
Khaled Saad <khaledsoultan63@gmail.com>
2021-01-24 17:44:08 +0000

    Added line between the two circles

--- assets/js/motion_model.js
+++ assets/js/motion_model.js
@@ -1,4 +1,5 @@
 var offset = [0,0];
+var line;
 class Robot
 {
     constructor(robot_id, robot_center_id, robot_edge_id)
@@ -70,6 +71,14 @@ document.addEventListener('mousemove', function(event) {
         const deg = rad*(180/Math.PI)*-1 + 180;
         rotateElement(robot_after.robot, deg)
     }
+    
+    const before_box = robot_before.robot.getBoundingClientRect()
+    x1 = before_box.left + before_box.width/2 - 7.5;
+    y1 = before_box.top + before_box.height/2 - 7.5;
+    const after_box = robot_after.robot.getBoundingClientRect()
+    x2 = after_box.left + after_box.width/2 - 7.5;
+    y2 = after_box.top + after_box.height/2 - 7.5;
+    line.plot(x1, y1, x2, y2);
 }, true);
 
 function rotateElement(elem, deg)
@@ -79,4 +88,14 @@ function rotateElement(elem, deg)
     elem.style.msTransform     = 'rotate('+deg+'deg)';
     elem.style.oTransform      = 'rotate('+deg+'deg)';
     elem.style.transform       = 'rotate('+deg+'deg)';
+}
+
+function drawLine()
+{
+    var draw = SVG().addTo('body').size(1000, 1000)
+    line = draw.line(50, 51, 194, 51)
+    line.stroke({ color: 'black', width: 2, linecap: 'round' })
+    // arc = draw.path(f_svg_ellipse_arc([50,50],45,[-Math.PI/2,Math.PI/2],0));
+    // arc.fill('none');
+    // arc.stroke({ color: '#f06', width: 2, linecap: 'round', linejoin: 'round' });
 }
